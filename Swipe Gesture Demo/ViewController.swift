8cfa5e95d1a3ae6d19889b7240f987cdd2acdb0c
Mirko Cukich <mirkocukich@gmail.com>
2019-07-28 19:21:37 -0500

    IBOutlet connections made on ViewController

--- Swipe Gesture Demo/ViewController.swift
+++ Swipe Gesture Demo/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var swipeLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
+    
+    
 
 }
 
